19e1704248893a574271b427c83e7f6d89b173b0
Meno Abels <meno.abels@adviser.com>
2020-10-06 10:54:02 +0000

    enable neckless as github-action

---
+++ github-action.js
@@ -0,0 +1,87 @@
+const core = require("@actions/core");
+const uuid = require("uuid");
+const { spawn } = require("child_process");
+const fs = require("fs").promises;
+const http = require("http");
+const https = require("https");
+
+function download(url) {
+  return new Promise((rs, rj) => {
+    let client = http;
+    if (url.toString().indexOf("https") === 0) {
+      client = https;
+    }
+    client
+      .request(url, (response) => {
+        response.on("error", (e) => {
+          rj(e);
+        });
+        const data = new Stream();
+        response.on("data", function (chunk) {
+          data.push(chunk);
+        });
+        response.on("end", function () {
+          rs(data.read());
+        });
+      })
+      .end();
+  });
+}
+
+function getTempDirectory() {
+  const tempDirectory = process.env["RUNNER_TEMP"] || ".";
+  ok(tempDirectory, "Expected RUNNER_TEMP to be defined");
+  return tempDirectory;
+}
+
+async function main() {
+  try {
+    const version = core.getInput("version");
+    const url = core.getInput("url");
+    const filename = core.getInput("filename");
+    let os = core.getInput("os");
+    let cpu = core.getInput("cpu");
+    switch (process.platform) {
+      case "darwin":
+        os = "mac";
+        cpu = undefined;
+        break;
+      case "linux":
+        os = "linux";
+        switch (process.arch) {
+          case "x64":
+            cpu = "amd64";
+            break;
+          case "arm":
+            cpu = "arm-v7";
+            break;
+          case "arm64":
+            cpu = "arm-v8";
+            break;
+        }
+        break;
+    }
+    let necklessUrl = `${url}/${version}/${filename}-${os}`;
+    if (cpu) {
+      necklessUrl = `${necklessUrl}-${cpu}`;
+    }
+    core.info(`Fetch neckless from:[${necklessUrl}]`)
+    const necklessBin = await download(necklessUrl);
+    const dir = path.join(getTempDirectory(), "neckless-bin");
+    await fs.mkdir(dir, {
+      recursive: true,
+      mode: 0o755,
+    });
+    const necklessFname = path.join(dir, "neckless");
+    await fs.writeFile(necklessFname, necklessBin);
+    await fs.chmod(necklessFname, 0o755);
+    core.exportVariable("NECKLESS_URL", necklessUrl);
+    core.exportVariable("NECKLESS_FNAME", necklessFname);
+    core.addPath(dir);
+    core.info(`Installed neckless into:[${necklessFname}]`)
+  } catch (e) {
+    core.setFailed(e);
+  }
+}
+
+main();
--- symmetric/symmetric.ts
+++ symmetric/symmetric.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto'
 import { CreateRandomKey, RawKey } from '../key/key'
-import { Fault, Ok, Result } from '../utils/render'
+import { Fault, Ok, Result, toBufferUint8, uint8Equal } from '../utils/render'
 
 export class KeyAndNonce {
 	constructor(
@@ -47,16 +47,48 @@ export interface SealRequest {
 	Checksum : Uint8Array
 }
 
-function Checksum(sr:SealRequest) :SealRequest {
+/*
+// example 96-bit nonce
+let nonce = Buffer.alloc(12, 0xff);
+
+// example 256-bit key
+let key = Buffer.alloc(32, 0x01);
+
+// some associated data
+let assocData = Buffer.alloc(16, 0xaa);
+
+// some data to encrypt
+let data = Buffer.alloc(64, 0xbb);
+
+// construct the cipher
+let cipher = crypto.createCipheriv('chacha20-poly1305', key, nonce, { authTagLength: 16 });
+
+// add associated data to cipher
+cipher.setAAD(assocData);
+
+// encrypt the data which will return an encrypted Buffer
+// that is of equal length to the overall input to the
+// stream cipher
+cipher.update(data);
+// 25805b670d5834ecb8a018ea87b6ff864117762481880fc723690d0e2d0cfd08a43c144291eb2df148b0d6981b66ca101344ea27c7a0860c2e5f1a7eed1e70eb
+
+// finalize cipher which allows us to calculate the MAC
+cipher.final();
+
+// obtain the 128-bit poly1305 MAC that includes our associated data
+cipher.getAuthTag()
+// 9ef622cec7a5719261031e9ca91049d4
+*/
+
+export function Checksum(sr:SealRequest) :SealRequest {
 	const my = crypto.createHash('sha256')
 	my.update(sr.Payload)
 	my.update(sr.Key.As32Byte())
 	sr.Checksum = toBufferUint8(my.digest())
-	// fmt.Printf("Seal:%x=>%x\n", sr.Checksum, data)
 	return sr
 }
 
-function nonce(aead cipher.AEAD, n : Uint8Array) : Uint8Array {
+function nonce(aead: cipher.AEAD, n : Uint8Array) : Uint8Array {
 	csum := make([]byte, aead.NonceSize())
 	if len(n) < aead.NonceSize() {
 		copy(csum, n)
@@ -88,18 +120,46 @@ export interface OpenContainer {
 export type VerifyFN = (x: Uint8Array, raw: RawKey, u: Uint8Array) => boolean;
 
 export function Verify(csum: Uint8Array, key: RawKey, open: Uint8Array): boolean  {
-	data := append(*open, key[:]...)
-	// fmt.Printf("Verify:%x=>%x", csum, data)
-	tmp := sha256.Sum256(data)
-	return bytes.Equal(tmp[:], csum)
+	const my = crypto.createHash('sha256')
+	my.update(open)
+	my.update(key.As32Byte())
+	return uint8Equal(toBufferUint8(my.digest()), csum)
 }
 
 export function SkipVerify(csum : Uint8Array, key : RawKey, open : Uint8Array) : boolean {
 	return true
 }
 
+class xchacha20poly1305 {
+	key = new Uint32Array(8)
+}
+
+// NewX returns a XChaCha20-Poly1305 AEAD that uses the given 256-bit key.
+//
+// XChaCha20-Poly1305 is a ChaCha20-Poly1305 variant that takes a longer nonce,
+// suitable to be generated randomly without risk of collisions. It should be
+// preferred when nonce uniqueness cannot be trivially ensured, or whenever
+// nonces are randomly generated.
+function newX(key: Uint8Array): Result<xchacha20poly1305> {
+	const ret = new xchacha20poly1305()
+	if (key.length != ret.key.length) {
+		return Fault("chacha20poly1305: bad key length")
+	}
+	const buf = Buffer.from(key)
+	ret.key[0] = buf.readInt32LE(0)
+	ret.key[1] = buf.readInt32LE(4)
+	ret.key[2] = buf.readInt32LE(8)
+	ret.key[3] = buf.readInt32LE(12)
+	ret.key[4] = buf.readInt32LE(16)
+	ret.key[5] = buf.readInt32LE(20)
+	ret.key[6] = buf.readInt32LE(24)
+	ret.key[7] = buf.readInt32LE(28)
+	return Ok(ret)
+}
+
 export function Open(key: RawKey, sc :SealedContainer, verify: VerifyFN): Result<OpenContainer> {
-	aead, err := chacha20poly1305.NewX(key[:])
+	const aead = newX(key.As32Byte())
+	// aead, err := chacha20poly1305.NewX(key[:])
 	if err != nil {
 		return nil, err
 	}
@@ -124,13 +184,12 @@ export interface Base64SealContainer extends KeyAndNonce {
 	Payload:  string
 }
 
-
 export function OpenBase64(key: RawKey, pp: Base64SealContainer, verify: VerifyFN): Result<OpenContainer> {
-	plain, err := base64.RawStdEncoding.DecodeString(pp.Payload)
-	if err != nil {
-		return nil, err
+	const plain = Buffer.from(pp.Payload, 'base64')
+	if (!plain) {
+		return Fault("decoding error of Payload")
 	}
-	return Open(key, &SealedContainer{
+	return Open(key, {
 		Checksum: pp.Checksum,
 		Payload:  plain,
 	}, verify)
